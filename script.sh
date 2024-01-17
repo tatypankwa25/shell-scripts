fa84d71a60ca151c2471030fba4cfcaf47a3a843
claudedevops <visionary_p@yahoo.com>
2024-01-17 04:24:58 +0000

    first commits

---
+++ script.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+###################################
+# Author: Claude
+# Date: Jan 15, 2024
+##################################
+
+set -x
+
+# create deployment directory
+mkdir deployment
+
+# change directory
+cd deployment
+
+# create a file called sam.txt
+touch sam.txt
+
+# give full access to owner
+sudo chmod 744 sam.txt
+
+# create a user sam
+sudo useradd sam
+
+# change ownernership of file
+sudo chown sam:sam sam.txt
+
+# create app directory
+#mkdir /home/ubuntu/app
+
+cd .. && mkdir app
+
+# copy file to app
+cp --preserve=sam:sam deployment/sam.txt app
